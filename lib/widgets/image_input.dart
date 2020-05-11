12a0772e0a0cca1bef1ae4bb23e20d792270cb1f
MehmetCelik4 <mehmetcelik4@gmail.com>
2020-05-11 21:03:47 +0300

    add place form

---
+++ lib/widgets/image_input.dart
@@ -0,0 +1,51 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class ImageINput extends StatefulWidget {
+  @override
+  _ImageINputState createState() => _ImageINputState();
+}
+
+class _ImageINputState extends State<ImageINput> {
+  File _storedImage;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Container(
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 1,
+              color: Colors.grey,
+            ),
+          ),
+          child: _storedImage != null
+              ? Image.file(
+                  _storedImage,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                )
+              : Text(
+                  "No image Taken",
+                  textAlign: TextAlign.center,
+                ),
+          alignment: Alignment.center,
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        Expanded(
+          child: FlatButton.icon(
+            icon: Icon(Icons.camera),
+            onPressed: () {},
+            label: Text("Take Picture"),
+            textColor: Theme.of(context).primaryColor,
+          ),
+        )
+      ],
+    );
+  }
+}
